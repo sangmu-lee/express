89cec9100f2ba1ce5f737ba4edeb101f596e0afc
sangmu-lee <sangmu-lee@codecrain.com>
2022-10-12 00:05:53 +0900

    init express

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
-const http = require("http");
-const fs = require("fs");
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === "/") {
-        res.write("<html>");
-        res.write("<head><title>Enter Message</title></head>");
-        res.write(
-            '<body><form action="/message" methods="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>'
-        );
-        res.write("</html>");
-        return res.end();
-    }
-    if (url === "/message" && method === "POST") {
-        fs.writeFileSync("message.txt", "DUMMY");
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-    }
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body><h1>Hello My Page</h1></body>");
-    res.write("</html>");
-    res.end();
-});
+const app = express();
 
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log('in another middleware!!');
+    res.send(`<h1>Hello World</h1>`)
+})
+
+app.listen(3000);
